484243713691aef85be136ac6c89f20e04f8f7cd
rxthew <richardtheumaster@gmail.com>
2022-10-04 23:53:05 +0200

    Enables rendering of messages and add to view

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,29 @@
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-]
+
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ]
+  res.render('index', { title: 'Miniboard', messages: messages });
 });
 
 module.exports = router;
 
 // GET 'new' route//
-router.get('/new',funciton(req,res,next){
+router.get('/new',function(req,res,next){
+  
 
 })
