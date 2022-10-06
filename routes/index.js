ef408413bf8639630b22c043f99d9cf6f280bad6
rxthew <richardtheumaster@gmail.com>
2022-10-06 21:15:57 +0200

    Finalise miniboard

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,44 @@
 var express = require('express');
 var router = express.Router();
 
+let messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ]
   res.render('index', { title: 'Miniboard', messages: messages });
 });
 
-module.exports = router;
-
 // GET 'new' route//
 router.get('/new',function(req,res,next){
-  
+  res.render('form')  
 
 })
+
+//POST re: 'new' route//
+
+router.post('', function(req,res,next){
+  const newMessage = req.body.newMessage
+  const newAuthor = req.body.newAuthor
+  messages = [...messages,{
+    text: req.body.newMessage,
+    user: req.body.newAuthor,
+    added: new Date()
+  }]
+
+  res.redirect('/')
+})
+
+module.exports = router;
+
+
